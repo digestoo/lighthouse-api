951abdf6d844e8aac1e2d3a6aef1d3575643bb80
Mateusz Rzepa <mrzepa89@gmail.com>
2018-08-25 09:38:37 +0200

    server
    node.js server

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# lighthouse-api
-Lighthouse API (dockerized)
+# Lighthouse API (dockerized)
+
+Dockerized version of Lighthouse (https://developers.google.com/web/tools/lighthouse/)
+
+### Run API 
+
+```bash
+git clone git@github.com:digestoo/lighthouse-api.git
+cd lighthouse-api
+npm install
+PORT=3000 npm start
+```
+
+## Making requests
+
+```bash
+curl -XGET -H "Content-Type: application/json" http://localhost:3000/<domain>
+```
+
+URL params:
+
+- `domain`
+
+## License
+
+MIT
---
+++ server.js
@@ -0,0 +1,47 @@
+const lighthouse = require('lighthouse');
+const chromeLauncher = require('chrome-launcher');
+
+const express = require('express')
+const Promise = require('bluebird');
+const app = express()
+
+const opts = {
+  logLevel: 'info',
+  chromeFlags: [
+    '--show-paint-rects',
+    '--headless',
+    '--disable-gpu',
+    '--user-data-dir=/tmp/chrome-test-profile',
+    '--disable-web-security',
+    '--disable-sandbox',
+    '--disable-dev-shm-usage'
+  ]
+};
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+chromeLauncher.launch({chromeFlags: opts.chromeFlags})
+.then(chrome => {
+  opts.port = chrome.port;
+  console.log('Browser loaded');
+})
+.catch(err => {
+  console.log(err);
+})
+
+app.get('/status', async (req, res) => {
+  res.json({});
+})
+
+app.all('/:domain', async (req, res) => {
+  return lighthouse('http://' + req.params.domain, opts)
+  .then(results => {
+    return res.json(results.lhr);
+  });
+})
+
+module.exports = app;
